a948714376d75e19b4055b9b26f16dc1a3561d60
dltjs0220 <rkrud0220@naver.com>
2021-11-17 16:49:23 +0900

    11월 17일 오후

--- HelloWorld/src/hello/App.java
+++ HelloWorld/src/hello/App.java
@@ -5,7 +5,7 @@ public class App { // 클래스 App 만들기
 	public static void main(String[] args) {
 		// 메인 메소드에서 프로그램 시작
 		// 주석	단축기 CTRL + /
-		System.out.println("헬로우");
+		System.out.println("헬로우~");
 		// 실행 방법 이클상단 ▶, 오른쪽 마우스 Run ad 자바 애플리케이션
 		// 왼쪽편 클래스파일을 선택하고 ▶ 오른쪽 마우스 Run ad 자바 애플리케이션
 		// CTRL + F11 		
